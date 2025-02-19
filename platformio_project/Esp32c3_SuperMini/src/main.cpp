f2861a4a90ec336f218e95151fc4e607b86d4dc0
pawel <pawel.herok@gmail.com>
2025-02-19 21:10:37 +0100

    added measuring for both sensors

--- platformio_project/Esp32c3_SuperMini/src/main.cpp
+++ platformio_project/Esp32c3_SuperMini/src/main.cpp
@@ -10,12 +10,10 @@
 #define PIN_XSHUT_RIGHT 2
 #define PIN_XSHUT_LEFT 3
 
-const double RANGE_MODIFIER = 0.006;
-const double RANGE_EXPONENT = 3.0;
-const int MEASUREMENTS_LEN = 3;
+#define SENSOR_MAX_RANGE_MM (short)2500
 
-Adafruit_VL53L0X sensor_right = Adafruit_VL53L0X(); // first sensor (the one with the black dot)
-Adafruit_VL53L0X sensor_left = Adafruit_VL53L0X(); // second sensor
+Adafruit_VL53L0X sensorRight = Adafruit_VL53L0X(); // first sensor (the one with the black dot)
+Adafruit_VL53L0X sensorLeft = Adafruit_VL53L0X(); // second sensor
 
 void playMelody()
 {
@@ -28,7 +26,7 @@ void playMelody()
   }
 }
 
-void pins_init()
+void pinsInit()
 {
   pinMode(PIN_BUZZER, OUTPUT);
   pinMode(PIN_XSHUT_RIGHT, OUTPUT);
@@ -41,7 +39,7 @@ void setup()
 {
   Serial.begin(9600); // whatever, usb speed anyway
 
-  pins_init();
+  pinsInit();
   Wire.begin(8, 9);
 
   playMelody();
@@ -50,25 +48,25 @@ void setup()
 
   //  // sensor 1 begin
   digitalWrite(PIN_XSHUT_LEFT, LOW);
-  while(!sensor_right.begin(0x30))
+  while(!sensorRight.begin(0x30))
   {
-    tone(PIN_BUZZER, 3000, 10);
-    delay(50);
+    tone(PIN_BUZZER, 2000, 20);
+    delay(100);
   }
 
   digitalWrite(PIN_XSHUT_LEFT, HIGH);
-  while(!sensor_right.begin(0x29))
+  while(!sensorLeft.begin(0x29))
   {
-    tone(PIN_BUZZER, 3000, 10);
-    delay(20);
-    tone(PIN_BUZZER, 3000, 10);
-    delay(50);
+    tone(PIN_BUZZER, 1000, 10);
+    delay(200);
+    tone(PIN_BUZZER, 1000, 50);
+    delay(1000);
   }
 
-  sensor_right.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
-  sensor_right.startRangeContinuous();
-  sensor_left.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
-  sensor_left.startRangeContinuous();
+  sensorRight.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
+  sensorRight.startRangeContinuous();
+  sensorLeft.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
+  sensorLeft.startRangeContinuous();
 
   for (uint8_t address = 1; address < 127; address++)
   {
@@ -79,8 +77,22 @@ void setup()
       Serial.println(address, HEX);
     }
   }
+
+  short rightRange = 0;
+  short leftRange = 0;
+  while (true)
+  {
+    if (sensorRight.isRangeComplete())
+    {
+      rightRange = min((short)sensorRight.readRange(), SENSOR_MAX_RANGE_MM);
+    }
+    if (sensorLeft.isRangeComplete())
+    {
+      leftRange = min((short)sensorLeft.readRange(), SENSOR_MAX_RANGE_MM);
+    }
+  }
 }
- 
+
 void loop()
 {
 
