49d2f48d2f6eeeaa99d41c08dcb2c0add0a78dd9
pawel <pawel.herok@gmail.com>
2025-02-19 20:47:26 +0100

    added support for 2 sensors

--- platformio_project/Esp32c3_SuperMini/src/main.cpp
+++ platformio_project/Esp32c3_SuperMini/src/main.cpp
@@ -4,17 +4,18 @@
 
 #include "wifi_config.h"
 
-#define PIN_LED_BUILTIN 8 // ACTUALLY ITS 8 INSTAED OF 7 THAT IS DEFINED IN LED_BUILTIN
-
-#define PIN_SDA_PIN 8
+#define PIN_SDA_PIN 8 // connected HIGH side to builtin LED
 #define PIN_SCL_PIN 9
 #define PIN_BUZZER 10
+#define PIN_XSHUT_RIGHT 2
+#define PIN_XSHUT_LEFT 3
 
 const double RANGE_MODIFIER = 0.006;
 const double RANGE_EXPONENT = 3.0;
 const int MEASUREMENTS_LEN = 3;
 
-Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
+Adafruit_VL53L0X sensor_right = Adafruit_VL53L0X(); // first sensor (the one with the black dot)
+Adafruit_VL53L0X sensor_left = Adafruit_VL53L0X(); // second sensor
 
 void playMelody()
 {
@@ -27,90 +28,56 @@ void playMelody()
   }
 }
 
+void pins_init()
+{
+  pinMode(PIN_BUZZER, OUTPUT);
+  pinMode(PIN_XSHUT_RIGHT, OUTPUT);
+  pinMode(PIN_XSHUT_LEFT, OUTPUT);
+  digitalWrite(PIN_XSHUT_RIGHT, HIGH);
+  digitalWrite(PIN_XSHUT_LEFT, HIGH);
+}
+
 void setup()
 {
   Serial.begin(9600); // whatever, usb speed anyway
-  pinMode(PIN_BUZZER, OUTPUT);
+
+  pins_init();
   Wire.begin(8, 9);
 
   playMelody();
 
-  // sensor 1 begin
+  // begin sensors
 
-  while(!sensor.begin(0x30))
+  //  // sensor 1 begin
+  digitalWrite(PIN_XSHUT_LEFT, LOW);
+  while(!sensor_right.begin(0x30))
   {
-    tone(PIN_BUZZER, 2000, 200);
-    delay(200);
-    tone(PIN_BUZZER, 2000, 200);
-    delay(5000);
+    tone(PIN_BUZZER, 3000, 10);
+    delay(50);
   }
 
-  for (uint8_t address = 1; address < 127; address++) {
+  digitalWrite(PIN_XSHUT_LEFT, HIGH);
+  while(!sensor_right.begin(0x29))
+  {
+    tone(PIN_BUZZER, 3000, 10);
+    delay(20);
+    tone(PIN_BUZZER, 3000, 10);
+    delay(50);
+  }
+
+  sensor_right.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
+  sensor_right.startRangeContinuous();
+  sensor_left.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
+  sensor_left.startRangeContinuous();
+
+  for (uint8_t address = 1; address < 127; address++)
+  {
     Wire.beginTransmission(address);
-    if (Wire.endTransmission() == 0) {
+    if (Wire.endTransmission() == 0)
+    {
       Serial.print("Found I2C device at 0x");
       Serial.println(address, HEX);
     }
-  }
-
-  sensor.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_LONG_RANGE);
-  sensor.startRangeContinuous();
-
-  int measurements[MEASUREMENTS_LEN] = {0};
-  int measurements_delta[MEASUREMENTS_LEN] = {0};
-  short measurements_position_counter = 0;
-  int avarge = 10;
-  int tempRange;
-  unsigned long start_time_ms;
-  while (true)
-  {
-    start_time_ms = millis();
-
-    while (millis() - pow(avarge*RANGE_MODIFIER, RANGE_EXPONENT) <= start_time_ms)
-    {
-      if (sensor.isRangeComplete())
-      {
-        tempRange = sensor.readRange();
-        tempRange = min(tempRange, 2500);
-        measurements[measurements_position_counter] = tempRange;
-        measurements_position_counter ++ ;
-        if (measurements_position_counter == MEASUREMENTS_LEN)
-        {
-          int sum = 0;
-          for (int i = 0; i < MEASUREMENTS_LEN; i++)
-          {
-            sum += measurements[i];
-          }
-          avarge = (float)sum / (float)MEASUREMENTS_LEN;
-          int max_delta = 0;
-          for (int i = 0; i < MEASUREMENTS_LEN; i++)
-          {
-            measurements_delta[i] = abs(measurements[i] - avarge);
-            max_delta = max(measurements_delta[i], max_delta);
-          }
-          for (int i = 0; i < MEASUREMENTS_LEN; i++)
-          {
-            if (measurements_delta[i] == max_delta)
-            {
-              measurements[i] = -1;
-              break;
-            }
-          }
-          sum = 0;
-          for (int i = 0; i < MEASUREMENTS_LEN; i++)
-          {
-            int val = measurements[i]!=-1?measurements[i]:0;
-            sum += val;
-          }
-          avarge = (float)sum / ((float)MEASUREMENTS_LEN - 1.0);
-          measurements_position_counter = 0;
-        }
-        Serial.println(avarge); // ~~~~~~~~~~~~~~~~~~~~~~
-      }
-
-    }
-    tone(PIN_BUZZER, 1000, 10);
-    delay(50);
   }
 }
  
